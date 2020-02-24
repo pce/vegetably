5f809289659d85aa5ede55737b76bbfdd6c9993b
pce <info@pc-e.org>
2020-02-24 23:37:13 +0100

    bemify boxtable

--- src/components/box_table.js
+++ src/components/box_table.js
@@ -14,7 +14,7 @@ const BoxTable = ({data, selected, size=12}) => {
     if (selected === index) {
       classNames = "boxtable__cell__current "
     }
-    classNames += "boxtable__cell month "
+    classNames += "boxtable__cell boxtable__cell__value "
     return cells.push(<div className={classNames}>{index+1}</div>)
   })
   table.push (<div className="boxtable__row">{cells}</div>)
@@ -25,22 +25,22 @@ const BoxTable = ({data, selected, size=12}) => {
     let values = data[val][0]
     let invalues = data[val][1]
     Array(xCells).fill(1).map((_, index)=> {
-      let classNames  = "cell "
+      let classNames  = "boxtable__cell "
       if (values.includes(index+1)) {
-        classNames += "outmonth "
+        classNames += "boxtable__cell__first "
       } else if (invalues.includes(index+1)) {
-        classNames += "inmonth "
+        classNames += "boxtable__cell__second "
       } else {
         if (selected === index) {
-          classNames += "curmonth "
+          classNames += "boxtable__cell__current "
         }
-        classNames += "month "
+        classNames += "boxtable__cell__value "
       }
       return cells.push(<div className={classNames}>{" "}</div>)
     })
-    return table.push (<div className="row">{cells}</div>)
+    return table.push (<div className="boxtable__row">{cells}</div>)
   })
-  return <div className="table">{table}</div>
+  return <div className="boxtable__table">{table}</div>
 }
 
 export { BoxTable }
